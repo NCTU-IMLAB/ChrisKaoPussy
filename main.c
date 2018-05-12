f6e8c694d2a827df516a4979234cdb53ae2ae687
timweichien <tim960622@gmail.com>
2018-05-12 21:27:48 +0800

    initial commit of test file

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdint.h>
+
+/*
+ * main.c
+ */
+uint_least8_t i=1;
+uint_least8_t j=11;
+
+int main(void) {
+    //int hh=88;
+
+    //for(;;){
+        //j=i+10;
+
+        j=0;
+        for(i=0;i<25;i++){
+            i=i+1;
+            j=j+1;
+        }
+    //}
+
+}
